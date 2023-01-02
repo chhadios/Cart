bd387211c53faee1aeaf228049084b081147b970
Abhishek <chhadios13@gmail.com>
2023-01-02 14:13:48 +0530

    first commit

---
+++ backend/products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://images.unsplash.com/photo-1607936854279-55e8a4c64888?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aXBob25lJTIwMTJ8ZW58MHx8MHx8&w=1000&q=80",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://img.giznext.com/assets/model/2/96/apple-iphone-12-986bf5e98daf0059117d2f08f5a5e7.jpg?width=360",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://images.unsplash.com/photo-1607936854279-55e8a4c64888?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aXBob25lJTIwMTJ8ZW58MHx8MHx8&w=1000&q=80",
+  },
+
+];
+
+module.exports = products;
